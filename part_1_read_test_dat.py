ee5745e5e62d1e92272a588adbe08564560883cb
Magian <magianlee@gmail.com>
2019-02-13 21:08:00 -0500

    everything needed for the assignment
    test.txt
    screenshot
    dat.py

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -1,7 +1,13 @@
-import cc_dat_utils
+from builtins import print
 
-#Part 1
+from cc_dat_utils import make_cc_data_from_dat
+
+
 input_dat_file = "data/pfgd_test.dat"
+data = make_cc_data_from_dat(input_dat_file)
+print(data)
+
+
 
 #Use cc_dat_utils.make_cc_data_from_dat() to load the file specified by input_dat_file
 #print the resulting data
